2fcb6051c390b713783681e315cdbf024895024e
Thilak Bodige <104753214+thilakbodige@users.noreply.github.com>
2025-03-02 11:37:22 +0530

    Add files via upload

---
+++ Arthematicoperator3.java
@@ -0,0 +1,14 @@
+class Arthematicoperator3 
+{
+	public static void main(String[] args) 
+	{
+		String a= "Hello";
+		int b = 20;
+		char c = 'A';
+		boolean d = true;
+		int e = 65;
+		System.out.println(b+a+d);
+		System.out.println(c+b+a);
+		System.out.println(b+c+d);
+	}
+}
