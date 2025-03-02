11e2ca0b2e599a8f46d6ac36f88e9b264cfb505d
Thilak Bodige <104753214+thilakbodige@users.noreply.github.com>
2025-03-02 11:37:03 +0530

    Add files via upload

---
+++ Arthematicoperator2.java
@@ -0,0 +1,13 @@
+class Arthematicoperator2 
+{
+	public static void main(String[] args) 
+	{
+		String a= "Hello";
+		int b = 20;
+		char c = 'A';
+		boolean d = true;
+		int e = 65;
+		System.out.println(d+b+a);
+		System.out.println(c+a);
+	}
+}
