4e558cb617989c7034f2e71643d78412d31958e7
Thilak Bodige <104753214+thilakbodige@users.noreply.github.com>
2025-03-02 11:37:42 +0530

    Add files via upload

---
+++ Arthemticoperator.java
@@ -0,0 +1,18 @@
+class  Arthemticoperator
+{
+	public static void main(String[] args) 
+	{
+		int a= 5;
+		boolean b= true;
+		char c = 'A';
+		int d = 10;
+		
+			System.out.println(a+b);
+			System.out.println(a-b);
+			System.out.println(a+c);
+			System.out.println(a*d);
+			System.out.println(d/a);
+			System.out.println(c%a);
+			
+	}
+}
