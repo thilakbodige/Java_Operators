10f462614d565502079b9500aba0895747cb4a23
Thilak Bodige <104753214+thilakbodige@users.noreply.github.com>
2025-03-04 13:50:21 +0530

    Add files via upload

---
+++ SumofNumbersEvenorOdd.java
@@ -0,0 +1,17 @@
+class SumofNumbersEvenorOdd 
+{
+	public static void main(String[] args) 
+	{
+		int sum=0;
+	for(int i=-11;i>=-21;i--){
+		sum+=i;
+	}
+	if(sum%2==0){
+		System.out.println("The Sum is Even "+sum);
+	}
+	else
+		{
+		System.out.println("The Sum is Odd "+sum);
+}
+	}
+}
