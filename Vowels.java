814712f724d4f4d9dad37778d76c8fb94f9dd410
Thilak Bodige <104753214+thilakbodige@users.noreply.github.com>
2025-03-08 09:56:36 +0530

    Add files via upload

---
+++ Vowels.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+class  Vowel
+{
+	public static void main(String[] args) 
+	{
+	  vowel();
+    }
+
+	public static void vowel()
+	{
+
+	for (char ch ='A';ch<='Z';ch++)
+	  {
+    if(ch=='A'||ch=='E'||ch=='I'||ch=='O'||ch=='U')
+		{
+       System.out.println("It is a Vowel");
+	    }
+
+	  }
+	}	
+}
