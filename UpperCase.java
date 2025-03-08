b289795ee3343b1b57730b91777dcb8d6bfb9933
Thilak Bodige <104753214+thilakbodige@users.noreply.github.com>
2025-03-08 09:57:09 +0530

    Add files via upload

---
+++ UpperCase.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+class UpperCase 
+{
+	public static void main(String[] args) 
+	{
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter the Character");
+		char ch = sc.next().charAt(0);
+		char result = (char)(ch+32);
+		System.out.println(result);
+	}
+}
