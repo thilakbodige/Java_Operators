5ed223d435a861da1940a1c94267e7816cfacce8
Thilak Bodige <104753214+thilakbodige@users.noreply.github.com>
2025-03-08 09:58:31 +0530

    Add files via upload

---
+++ LowercaseAlphabetorNot.java
@@ -0,0 +1,10 @@
+class LowercaseAlphabetorNot 
+{
+	public static void main(String[] args) 
+	{
+		char ch = 'g';
+		boolean Alpha = (ch>='a')&&(ch<='z');
+	String s = (Alpha) ? "It is Lowercase Alphabet" : "It is Not Lowercase Alphabet";
+		System.out.println(s);
+	}
+}
